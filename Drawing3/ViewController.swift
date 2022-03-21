bcfea583b67f69ba7e542a3be88fa3ba13fb128a
Thien.nguyen <thien.nguyen@g-days.net>
2022-03-21 13:31:38 +0700

    Initial Commit

---
+++ Drawing3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drawing3
+//
+//  Created by BSP Macbook on 3/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
