b6eb6e18a8e68144603070fc6366db4711874093
Thien.nguyen <thien.nguyen@g-days.net>
2022-03-24 10:03:57 +0700

    OK

--- Drawing3/ViewController.swift
+++ Drawing3/ViewController.swift
@@ -7,92 +7,83 @@
 
 import UIKit
 
-// New class canvas for build drawing
-class Canvas: UIView {
-    
-    override func draw(_ rect: CGRect) {
-        
-        // Customize drawing
-        super.draw(rect)
-        // End Customize
-        
-        guard let context = UIGraphicsGetCurrentContext() else { return }
-        
-//        // Here is the line
-//        // Declare var start and end
-//        let startPoint = CGPoint(x: 0, y: 0)
-//        let endPoint = CGPoint(x: 100, y: 100)
-//        // End declaring
-//
-//        // Declare local physicall
-//        context.move(to: startPoint)
-//        context.addLine(to: endPoint)
-//        // End declaring
-//        // End lining
-        
-        // Custom display of lines
-        context.setStrokeColor(UIColor.black.cgColor)
-        context.setLineWidth(5)
-        context.setLineCap(.butt)
-        // End customing
-        
-        lines.forEach { (line) in
-            for (i, p) in line.enumerated() {
-                if i == 0 {
-                    context.move(to: p)
-                } else {
-                    context.addLine(to: p)
-                }
-            }
-        }
-        
-        context.strokePath()
-    }
-    
-    // Declare the line to show
-//    var line = [CGPoint]()
-    var lines = [[CGPoint]]()
-    // End declaring
-    
-    // Start adding when touch it
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        lines.append([CGPoint]())
-    }
-    // End starting
-    
-    // Track the finger when moving
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // Declare co-ordinate
-        guard let point = touches.first?.location(in: nil) else { return }
-        // End declaring
-        
-        // Add point into line
-        guard var lastLine = lines.popLast() else { return }
-        lastLine.append(point)
-//        line.append(point)
-        // End Adding
-        
-        // Add lastline into biggest array
-        lines.append(lastLine)
-        // End adding
-        
-        setNeedsDisplay()
-    }
-    // End tracking
-}
-// End class
 
 class ViewController: UIViewController {
     
+    // Declare variable relative
+    // Declare draw
     let canvas = Canvas()
-
+    // End draw
+    // End declaring
+    
+    // Add undo, clear button and choosing color
+    // Undo button
+    let undoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Undo", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleUndo), for: .touchUpInside)
+        return button
+    }()
+    // End undo
+    
+    // func undo
+    @objc func handleUndo() {
+        canvas.undo()
+    }
+    // End func
+    
+    // Clear button
+    let clearButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Clear", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleClear), for: .touchUpInside)
+        return button
+    }()
+    // End clear
+    
+    // func clear
+    @objc func handleClear() {
+        print("Clear func")
+        canvas.clear()
+    }
+    // End func
+    // End Adding
+    
+    override func loadView() {
+        self.view = canvas
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.addSubview(canvas)
         canvas.backgroundColor = .white
-        canvas.frame = view.frame
+        
+        setLayouts()
+        // End Constrainting
     }
+    
+    // Set layout to showing
+    fileprivate func setLayouts() {
+        // Show undo, clear button
+        let stackView = UIStackView(arrangedSubviews: [
+            undoButton,
+            clearButton
+        ])
+        // End showing
+        
+        stackView.distribution = .fillEqually
+        
+        view.addSubview(stackView)
+        
+        // Constraint position of stackview to showing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    // End setting
 }
 
